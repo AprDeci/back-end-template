9d3431971855b4ee96bc13dfb34a0f75bc465b78
Aprdec <luchen126@gmail.com>
2025-12-24 10:54:57 +0800

    feat(core): 优化日志初始化，支持控制台和文件输出
    - 新增控制台输出配置，支持同时输出到控制台和文件
    - 根据全局配置决定是否写入文件日志
    - 使用 zapcore.NewTee 组合多路输出核心
    - 添加对标准输出流的支持
    - 保持默认日志级别为 Debug

--- gin/core/zap.go
+++ gin/core/zap.go
@@ -1,15 +1,35 @@
 package core
 
 import (
+	"gin-template/global"
+	"os"
+
 	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 func InitLogger() (logger *zap.Logger, err error) {
-	writeSyncer := getLogWriter()
+	//文件输出
+	fileWriteSyncer := getLogWriter()
+
 	encoder := getEncoder()
-	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+	//控制台输出
+	consoleWriteSyncer := zapcore.AddSync(os.Stdout)
+
+	//level
+
+	var core zapcore.Core
+
+	if global.GVA_CONFIG.Logger.WriteInFile {
+		core = zapcore.NewTee(zapcore.NewCore(encoder, fileWriteSyncer, zapcore.DebugLevel), // 文件
+			zapcore.NewCore(encoder, consoleWriteSyncer, zapcore.DebugLevel), // 控制台
+		)
+	} else {
+		core = zapcore.NewTee(zapcore.NewCore(encoder, consoleWriteSyncer, zapcore.DebugLevel)) // 控制台
+
+	}
+
 	logger = zap.New(core, zap.AddCaller())
 	return logger, nil
 }
